08e3ac516e0f6159502b243c9d1b1cfda9224cd5
esterfania <esterfaniasantos@gmail.com>
2021-12-07 22:24:37 -0300

    feat(client): update text

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon.png"></link>
         <link rel="apple-touch-icon" href="/img/icon.png"></link>
         <link rel="manifest" href="/manifest.json"></link>
-        <meta name="description" content="Um projeto simples para começar a trabalhar com React Avançados" />
+        <meta name="description" content="Um projeto simples para começar a trabalhar com React Avançado" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
